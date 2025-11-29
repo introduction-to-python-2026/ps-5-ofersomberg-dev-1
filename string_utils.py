e6d2764b4b61036c9c8f01d70ef7ea9bd630f2fa
ofersomberg-dev <ofersomberg@gmail.com>
2025-11-29 14:04:46 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -36,9 +36,8 @@ def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
     Example: 'H2O' → {'H': 2, 'O': 1}"""
 
-
+    atom_counts = {}
     for atom in split_before_uppercases(molecular_formula):
-        atom_counts = {}
         atom_name, atom_count = split_at_digit(atom)
         atom_counts[atom_name] = atom_counts.get(atom_name, 0) + atom_count
     return atom_counts
