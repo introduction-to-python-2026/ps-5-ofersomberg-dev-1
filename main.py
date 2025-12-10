76a3e193e0dc820f37119782d7e964233858d516
ofersomberg-dev <ofersomberg@gmail.com>
2025-12-10 07:39:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
     # 1.parse reaction
     reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
-    product_atoms = count_atoms_in_reaction(products)
+    reactant_atoms = count_atoms_in_molecule(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    product_atoms = count_atoms_in_molecule(products)
 
     # 2.build equation and solve
     equations, coefficients = build_equations(reactant_atoms, product_atoms)
