a026364e388a23a20db6550fdb2fdc0892e19d59
ofersomberg-dev <ofersomberg@gmail.com>
2025-12-10 07:55:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from string_utils import split_by_capitals, split_at_digit, parse_chemical_reaction, count_atoms_in_molecule, count_atoms_in_reaction
+from string_utils import split_before_uppercases, split_at_digit, parse_chemical_reaction, count_atoms_in_molecule, count_atoms_in_reaction
 from equation_utils import ELEMENTS, generate_equation_for_element, build_equations, my_solve
 from sympy import Eq, symbols, solve
 
