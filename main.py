d0c27b5280490a85860de5c5c106fbeb23dc39d9
ofersomberg-dev <ofersomberg@gmail.com>
2025-12-10 07:43:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from string_utils import split_by_capitals, split_at_digit, count_atoms_in_molecule
-
-from equation_utils import generate_equation_for_element, build_equations, my_solve
+from string_utils import split_by_capitals, split_at_digit, parse_chemical_reaction, count_atoms_in_molecule
+from equation_utils import ELEMENTS, generate_equation_for_element, build_equations, my_solve
+from sympy import Eq, symbols, solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
